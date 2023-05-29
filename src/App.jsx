d770344329535f0719133a9e27287d6315fca7a4
Ivan Stepancic <ivan.stepancic@yahoo.com>
2023-05-29 21:13:25 +0200

    Preconnecting to scaledrone. Input, Messages work in browser.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,90 @@
-function App() {
-  return <>
-  </>
-};
+import { Component } from "react";
+import React from "react";
+import Messages from "./Messages";
+import "./App.css";
+import Input from "./Input";
+
+
+
+function randomName() {
+  const adjectives = [
+    "gonzales",
+    "vjetar",
+    "stenac",
+    "svjedok",
+    "trkac",
+    "cudak",
+    "vozač",
+  ];
+  const nouns = [
+    "brzi",
+    "usporeni",
+    "zeleni",
+    "unaprijeđeni",
+    "labavi",
+    "crni",
+    "okomiti",
+    "tanki",
+    "debeli",
+  ];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 0xffffff) - toString(16);
+}
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+    messages: [
+      {
+        text: "Ovo je test poruka (hardkodirana)",
+        member: {
+          color: "blue",
+          username: "plaviorao",
+        },
+      },
+    ],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    },
+  }
+  }
+
+
+  onSendMessage = (message) => {
+    const messages = this.state.messages;
+    messages.push({
+      text: message,
+      member: this.state.member,
+    })
+    this.setState({ messages: messages })
+  };
+  
+  render(){
+    return ( <>
+
+    
+      <div className="App">
+        <div className="App-header">
+          <h1>chitChat - easy chat App</h1>
+        </div>
+
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+        <Input onSendMessage={this.onSendMessage} />
+      </div>
+      </>
+    );
+  }
+}
 
 export default App;
